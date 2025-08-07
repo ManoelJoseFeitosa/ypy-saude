cd24bfcae7799a9833de6d79d4bad306036badcf
Herick Kawan <herickkawansilva@gmail.com>
2025-08-07 15:46:46 -0300

    feat: Adicionado blocozapsing em services.php

--- config/services.php
+++ config/services.php
@@ -6,12 +6,6 @@ return [
     |--------------------------------------------------------------------------
     | Third Party Services
     |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
     */
 
     'postmark' => [
@@ -35,9 +29,13 @@ return [
         ],
     ],
 
-    // --- BLOCO ADICIONADO ---
     'mercadopago' => [
         'token' => env('MERCADOPAGO_TOKEN'),
+    ],
+
+    // --- BLOCO ADICIONADO ---
+    'zapsign' => [
+        'token' => env('ZAPSIGN_TOKEN'),
     ],
 
 ];
